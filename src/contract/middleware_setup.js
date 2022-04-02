ad220d3c86fb280b3959a491eb42c0b6ec8a1534
yuriderozan <yuriderozan@gmail.com>
2022-04-02 18:57:29 +1100

    update contract to 9.2

--- src/contract/middleware_setup.js
+++ src/contract/middleware_setup.js
@@ -20,7 +20,7 @@ var Contract = require('web3-eth-contract');
 Contract.setProvider(RPC);
 
 //replace contract address as needed...
-var contractAddress = '0x7592d4C73c910867b84212C64FB0dD9815f5c0D6';
+var contractAddress = '0x00f2967e9bC4Def83cD87407678D0C6035c19813';
 var contract = new Contract(ABI, contractAddress);
 
 export { Web3, RPC, chainID, web3, temp_account, ABI, Contract, contractAddress, contract };
